97304d5691f29f4950100964048c5d5de4b95323
lucapolzone <lucapolzone@gmail.com>
2024-02-25 19:43:43 +0100

    feat: add static result page

---
+++ resultpage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="it-IT">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Luca Polzone" />
+  <meta name="description" content="Esercizio - PHP Badwords" />
+  <title>Esercizio - PHP Badwords</title>
+  <!-- Bootstrap link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  
+  <div class="w-50 mx-auto mt-5">
+      <div class="col-12">
+        <h2>Questa è la tua frase:</h2>
+        <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo temporibus consequuntur sit voluptatem dolor earum ea maiores eveniet vel, aliquid voluptatum molestias perferendis iusto impedit officiis, fugit sint veritatis necessitatibus.</p>
+        <h3>La tua frase è lunga <span class="fw-normal">tot</span> caratteri</h3>
+      </div>
+      
+      <div class="col-12 mt-5">
+        <h2>Questa è la tua frase censurata:</h2>
+        <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo temporibus consequuntur sit voluptatem dolor earum ea maiores eveniet vel, aliquid voluptatum molestias perferendis iusto impedit officiis, fugit sint veritatis necessitatibus.</p>
+        <h3>La tua frase è lunga <span class="fw-normal">tot</span> caratteri</h3>
+      </div>
+  </div>
+
+</body>
+</html>
